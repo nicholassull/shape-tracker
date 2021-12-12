13a4e68e5c77588246650a1bdbfddea5fe6f2585
Nick Sullivan <nicholassull@gmail.com>
2021-12-12 12:55:05 -0800

    add eslint

--- src/triangle.js
+++ src/triangle.js
@@ -7,15 +7,3 @@ export default function Triangle(side1, side2, side3) {
 Triangle.prototype.checkType = function() {
   return "I can't answer that yet!";
 }
-
-$(document).ready(function() {
-  $('#triangle-checker-form').submit(function(event) {
-    event.preventDefault();
-    const length1 = $('#length1').val();
-    const length2 = $('#length2').val();
-    const length3 = $('#length3').val();
-    const triangle = new Triangle(length1, length2, length3)
-    const response = triangle.checkType();
-    $('#response').append("<p>" + response + "</p>");
-  })
-})
