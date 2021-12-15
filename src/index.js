27be2001eef4d0da74fe74ce3e8db1ad9939cd62
Nick Sullivan <nicholassull@gmail.com>
2021-12-15 12:25:41 -0800

    add UI logic for #rectangle-area-form

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import Triangle from "./triangle";
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Triangle from "./js/triangle.js";
+import Rectangle from "./js/rectangle.js"
 
 $(document).ready(function() {
   $('#triangle-checker-form').submit(function(event) {
@@ -13,5 +14,14 @@ $(document).ready(function() {
     const triangle = new Triangle(length1, length2, length3);
     const response = triangle.checkType();
     $('#response').append("<p>" + response + "</p>");
+  });
+
+  $('#rectangle-area-form').submit(function(event) {
+    event.preventDefault();
+    const length1 = parseInt($('#rect-length1').val());
+    const length2 = parseInt($('#rect-length2').val());
+    const rectangle = new Rectangle(length1, length2);
+    const response = rectangle.getArea();
+    $('#response2').append(`<p> The area of the rectangle is ${response}. </p>`);
   });
 });
